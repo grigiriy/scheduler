a7ba39b50c0a5563210041b330bf9b8074c9e073
Grigiriy <grigiriy.malyshev@gmail.com>
2020-04-23 14:18:59 +0300

    layout fixes on lesson

--- single-lessons.php
+++ single-lessons.php
@@ -84,15 +84,12 @@ document.location.href = '<?= array_shift($childrens)->guid; ?>';
 ?>
 
 <div data-can_add="<?= $is_time_to_add === true ? 'true' : '' ;?>"
-    data-learning="<?= $is_learning === true ? 'true' : '' ;?>"
-    data-new_post_id="<?= get_boundary_post( false, '', false )[0]->ID+1 ?>">
-    <div class="row">
-        <div class="col-12">
-            <h1 class="d-flex" data-id="<?= $yt_code ?>">
-                <?= get_the_title(); ?>
-            </h1>
+    data-learning="<?= $is_learning === true ? 'true' : '' ;?>" class="col-12">
+    <h1 class="d-flex" data-id="<?= $yt_code ?>">
+        <?= get_the_title(); ?>
+    </h1>
 
-            <?php
+    <?php
             set_query_var( 'is_favor', $is_favor );
             set_query_var( 'is_time_to_add', $is_time_to_add );
             set_query_var( 'is_passed', $is_passed );
@@ -113,41 +110,40 @@ document.location.href = '<?= array_shift($childrens)->guid; ?>';
             ?>
 
 
-            <?php get_template_part('theme-helpers/template-parts/lesson','buttons');  ?>
+    <?php get_template_part('theme-helpers/template-parts/lesson','buttons');  ?>
 
 
-        </div>
-        <div id="player" class="mb-5 col-12"></div>
-        <?php
+</div>
+<div id="player" class="mb-5 col-12"></div>
+<?php
         $type = get_the_terms( $post_id, 'course_type' )[0]->slug;
         if( $type === 'with-teacher' ) {
             $detailed_sentences = carbon_get_post_meta($post_id, 'detailed_sentences'); ?>
-        <div class="col-12">
-            <table class="table table-dark">
-                <tbody>
-                    <?php foreach($detailed_sentences as $detailed_sentence){ ?>
-                    <tr>
-                        <td onclick="show_hint(this)"><?= $detailed_sentence['sentence'] ?></td>
-                        <td style="display:none"><?= $detailed_sentence['note_1'] ?></td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-        <?php
+<div class="col-12">
+    <table class="table table-dark">
+        <tbody>
+            <?php foreach($detailed_sentences as $detailed_sentence){ ?>
+            <tr>
+                <td onclick="show_hint(this)"><?= $detailed_sentence['sentence'] ?></td>
+                <td style="display:none"><?= $detailed_sentence['note_1'] ?></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+<?php
 
         } else if( $type === 'self'  ) {
 
         ?>
-        <div class="pdf col-12">
-            <embed src="/wp-content/uploads/<?= $pdf; ?>" width="100%" height="470px" />
-            <div class="d-flex justify-content-between">
-                <a target="_blank" href="/wp-content/uploads/<?= $pdf; ?>">Open in new window</a>
-                <a download="<?= get_the_title(); ?> PDF" target="_blank"
-                    href="/wp-content/uploads/<?= $pdf; ?>">Download</a>
-            </div>
-        </div>
-        <!-- <a href="http://www.google.com/calendar/event?
+<div class="pdf col-12">
+    <embed src="/wp-content/uploads/<?= $pdf; ?>" width="100%" height="470px" />
+    <div class="d-flex justify-content-between">
+        <a target="_blank" href="/wp-content/uploads/<?= $pdf; ?>">Open in new window</a>
+        <a download="<?= get_the_title(); ?> PDF" target="_blank" href="/wp-content/uploads/<?= $pdf; ?>">Download</a>
+    </div>
+</div>
+<!-- <a href="http://www.google.com/calendar/event?
             action=TEMPLATE
             &text=event-title
             &dates=[start-custom format='Ymd\\THi00\\Z']/[end-custom format='Ymd\\THi00\\Z']
@@ -156,109 +152,107 @@ document.location.href = '<?= array_shift($childrens)->guid; ?>';
             &trp=false
             &sprop=
             &sprop=name:" target="_blank" rel="nofollow">Add to my calendar</a> -->
-        <?php } ?>
+<?php } ?>
+</div>
+<script>
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+
+var player_place = document.getElementById('player');
+player_place.parentNode.insertBefore(tag, player_place);
+var player;
+var yt_code = document.querySelector('h1').getAttribute('data-id');
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '500px',
+        width: '100%',
+        videoId: yt_code,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+    console.log('cc -> ', player.getOptions('cc'));
+}
+
+function onApiChange(event) {
+    console.log('something changed...');
+}
+
+function onPlayerReady(event) {
+    // event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    console.log('cc -> ', player.getOptions('cc'));
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+</script>
+
+<?php
+if(
+    get_the_author_meta('ID') === $user_id &&
+    $post->post_parent === 0
+){
+?>
+<div class="card mt-5">
+    <div class="card-header">
+        <h3>Edit lesson info</h3>
     </div>
-    <script>
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-
-    var player_place = document.getElementById('player');
-    player_place.parentNode.insertBefore(tag, player_place);
-    var player;
-    var yt_code = document.querySelector('h1').getAttribute('data-id');
-
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            height: '500px',
-            width: '100%',
-            videoId: yt_code,
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-        console.log('cc -> ', player.getOptions('cc'));
-    }
-
-    function onApiChange(event) {
-        console.log('something changed...');
-    }
-
-    function onPlayerReady(event) {
-        // event.target.playVideo();
-    }
-
-    function onPlayerStateChange(event) {
-        console.log('cc -> ', player.getOptions('cc'));
-    }
-
-    function stopVideo() {
-        player.stopVideo();
-    }
-    </script>
-
-    <?php
-    if(
-        get_the_author_meta('ID') === $user_id &&
-        $post->post_parent === 0
-    ){
-    ?>
-    <div class="card mt-5">
-        <div class="card-header">
-            <h3>Edit lesson info</h3>
-        </div>
-        <div class="card-body lesson_edit">
-            <button class="btn"><a href="/add_post/?rcl-post-edit=<?= $post->ID; ?>">Edit Post</a></button>
-        </div>
+    <div class="card-body lesson_edit">
+        <button class="btn"><a href="/add_post/?rcl-post-edit=<?= $post->ID; ?>">Edit Post</a></button>
     </div>
-    <button id="lesson_passed" class="btn btn-success">
-        <h4 class='mb-0'>Finish</h4>
-    </button>
-    <?php }
+</div>
+<button id="lesson_passed" class="btn btn-success">
+    <h4 class='mb-0'>Finish</h4>
+</button>
+<?php }
         if ($is_learning && !empty($less_vals)) {
             if ($current_lesson_val==='0' || ( intval($current_lesson_val)>0 && $now_incTZ>=$less_vals[intval($current_lesson_val)-1]) ) {
     ?>
-    <div class="card mt-5">
-        <?php if($is_lastLesson){ ?>
-        <div class="card-header">
-            <h3>Congratulations! This is the last reiteration of this lesson</h3>
-        </div>
-        <?php } ?>
-        <div class="card-body">
-            <button id="lesson_passed" class="btn btn-success" data-last=<?= $is_lastLesson ? 'true' : 'false' ?>>
-                <h4 class='mb-0'>Finish <?= $is_lastLesson ? 'course!' : 'practice' ?></h4>
-            </button>
-        </div>
+<div class="card mt-5">
+    <?php if($is_lastLesson){ ?>
+    <div class="card-header">
+        <h3>Congratulations! This is the last reiteration of this lesson</h3>
     </div>
-    <?php }
+    <?php } ?>
+    <div class="card-body">
+        <button id="lesson_passed" class="btn btn-success" data-last=<?= $is_lastLesson ? 'true' : 'false' ?>>
+            <h4 class='mb-0'>Finish <?= $is_lastLesson ? 'course!' : 'practice' ?></h4>
+        </button>
+    </div>
+</div>
+<?php }
     }
     if($is_time_to_add || $is_learning  || empty($list)) { ?>
-    <div class=" modal fade" id="add_lesson" tabindex="-1" role="dialog" aria-labelledby="add_lesson__label"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title"><?= get_the_title(); ?></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    lorem ipsum
-                    <?= (isset(get_user_meta($user_id)['frequency'])) ? get_user_meta($user_id)['frequency'][0] : 'Norm'; ?>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="mx-auto btn btn-success" id="add_course">Start
-                        learning!</button>
-                </div>
+<div class=" modal fade" id="add_lesson" tabindex="-1" role="dialog" aria-labelledby="add_lesson__label"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><?= get_the_title(); ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                lorem ipsum
+                <?= (isset(get_user_meta($user_id)['frequency'])) ? get_user_meta($user_id)['frequency'][0] : 'Norm'; ?>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="mx-auto btn btn-success" id="add_course">Start
+                    learning!</button>
             </div>
         </div>
     </div>
-    <?php
-    } ?>
-
 </div>
+<?php
+    } ?>
 
 <?php endwhile; };?>
 
