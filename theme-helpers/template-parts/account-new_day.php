c949e92b52520f3c662f8bd812117168fcfafa63
Grigiriy <grigiriy.malyshev@gmail.com>
2020-05-21 12:22:16 +0300

    exclude excess button on new_day block

--- theme-helpers/template-parts/account-new_day.php
+++ theme-helpers/template-parts/account-new_day.php
@@ -11,8 +11,7 @@
             </div>
 
             <div class="d-flex justify-content-around pt-5 my-5">
-                <a href="/courses/" class="btn btn-warning btn-round py-3 px-4">Add next material</a>
-                <a href="/courses/" class="btn btn-primary btn-round py-3 px-4">Choose next material</a>
+                <a href="/courses/" class="btn btn-warning btn-round py-3 px-5">Add next material</a>
             </div>
         </div>
     </div>
