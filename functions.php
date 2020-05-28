0f695af165f107097b9c563d1586c371d970a64d
Grigiriy <grigiriy.malyshev@gmail.com>
2020-05-28 18:09:46 +0300

    pushes start

--- functions.php
+++ functions.php
@@ -653,3 +653,79 @@ function set_scheduleMail($timer,$user_id,$post_id) {
 }
 // AND SET TIMERS
 // AND CALL A FUNCTION TO SEND LETTERS
+
+
+
+
+
+
+
+
+
+
+
+
+function sendMessage() {
+  $content      = array(
+      "en" => 'English Message'
+  );
+  $hashes_array = array();
+  array_push($hashes_array, array(
+      "id" => "like-button",
+      "text" => "Like",
+      "icon" => "http://i.imgur.com/N8SN8ZS.png",
+      "url" => "https://yoursite.com"
+  ));
+  array_push($hashes_array, array(
+      "id" => "like-button-2",
+      "text" => "Like2",
+      "icon" => "http://i.imgur.com/N8SN8ZS.png",
+      "url" => "https://yoursite.com"
+  ));
+  $fields = array(
+      'app_id' => "c511d27e-186f-48ed-b001-560c997c2387",
+      'included_segments' => array(
+          'All'
+      ),
+      'data' => array(
+          "foo" => "bar"
+      ),
+      'contents' => $content,
+      'web_buttons' => $hashes_array
+  );
+  
+  $fields = json_encode($fields);
+  print("\nJSON sent:\n");
+  print($fields);
+  
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json; charset=utf-8',
+      'Authorization: Basic NTNmNTU0OTUtMThkZi00YmI5LTg3ZTctMTFhYjA1YmJlNjY4'
+  ));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_HEADER, FALSE);
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  
+  $response = curl_exec($ch);
+  curl_close($ch);
+  
+  return $response;
+}
+
+
+// $response = sendMessage();
+// $return["allresponses"] = $response;
+// $return = json_encode($return);
+
+// $data = json_decode($response, true);
+// print_r($data);
+// $id = $data['id'];
+// print_r($id);
+
+// print("\n\nJSON received:\n");
+// print($return);
+// print("\n");
